477d31c996b74658b2ad690124d53abe840023aa
nihadjafarov01 <mi72kv3d0@code.edu.az>
2023-10-18 17:37:29 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, -2, 3, -4, -5, 6, -7 };
+            ConvertToPositive(ref array);
+        }
+        static void ConvertToPositive(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    array[i] = -array[i];
+                }
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
